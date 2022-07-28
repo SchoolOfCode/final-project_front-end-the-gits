9e4e2aa1c88727b401e2a71c80bcf65fbd8d6bd9
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2022-07-28 12:22:52 +0100

    basic Shopping List page

--- our-house/pages/ShoppingList.js
+++ our-house/pages/ShoppingList.js
@@ -1,10 +1,27 @@
 import React from 'react'
 import styles from '../styles/ShoppingList.module.css'
+import Navbar from '../components/Navbar.js'
+import InputBar from '../components/InputBar.js'
+import ShoppingListItem from '../components/ShoppingListItem.js'
 
 
 const ShoppingList = () => {
+  const props = {
+    name: "Lidl"
+  }
+  const items = ["bread", "milk", "chocolate"]
+
   return (
-    <div>ShoppingList</div>
+    <div className={styles.shoppinglist}>
+    <Navbar/>
+    <h1>{props.name} Shopping List</h1>
+    <InputBar/>
+    <ul>
+      {items.map((item) => (
+        <ShoppingListItem name={item} key={item.id}/>))}
+    </ul>
+
+    </div>
   )
 }
 
