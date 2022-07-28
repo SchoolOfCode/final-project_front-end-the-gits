c91257a2b659d47bf7c0ccca512d29b10ec3c3c5
leeconroy <leeconroy1977@yahoo.co.uk>
2022-07-28 15:55:29 +0100

    Home basic set up

--- our-house/pages/index.js
+++ our-house/pages/index.js
@@ -1,10 +1,26 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+// import  Logo from '../public/logo.svg'  
 
 export default function Home() {
   return (
-    <div>Home Page</div>
+    <div className={styles.home}>
+      <div className={styles.left}>
+        <h1>Our House</h1>
+        <p>Your one stop shop</p>
+        <p>to organise your home</p>
+        <div className={styles.buttons}>
+         <button>Login</button>
+         <button className={styles.signup}>Sign Up</button>
+        </div>
+      </div>
+      
+      <div className={styles.right}>
+      
+        <img src="/logo3.svg" width={760} height={940} alt="" />
+      </div>
+    </div>
       
   )
 }
