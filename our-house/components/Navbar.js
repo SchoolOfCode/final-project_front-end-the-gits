f69d11c3a14c24c8abec445e0782531ec58060ce
MumtazO <mumtazob@gmail.com>
2022-07-28 10:49:59 +0100

    navbar added with placeholder links

--- our-house/components/Navbar.js
+++ our-house/components/Navbar.js
@@ -1,14 +1,44 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-    <a>
-    <img src="/vercel.svg"></img>
-    </a>
+      <a>
+        <img src="/vercel.svg"></img>
+      </a>
+      <ul>
+        <li>
+          <Link href="/shopname">
+            <a>Shop Name</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/shoppinglist">
+            <a>Shopping List</a>
+          </Link>
+        </li>
+        <li>
+        <Link href="/userhome">
+            <a>User Home</a>
+          </Link>
+          </li>
+          
+        <li>
+          <Link href="/shoppinglist">
+            <a>Shopping List</a>
+          </Link>
+        </li>
+        <li>
+        <Link href="/userhome">
+            <a>User Home</a>
+          </Link>
+        </li>
+      </ul>
     </nav>
-   
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
+
+//homepage/shoppinglist/tesco
