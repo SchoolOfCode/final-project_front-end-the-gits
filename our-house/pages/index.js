cf2c22577777f7f2c026aa3071b052400217ab93
MumtazO <mumtazob@gmail.com>
2022-07-29 15:12:54 +0100

    attempt at avoiding conflict with indexjs file

--- our-house/pages/index.js
+++ our-house/pages/index.js
@@ -1,15 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import Navbar from '../components/Navbar'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div>
-      <div>
-        <h2>HELLO</h2>
-        <p>Hellow there lets try to move the navbar to one side of this text</p>
-      </div>
     </div>
   )
 }
