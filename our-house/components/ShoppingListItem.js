4f9d84c9205ed2d02ef1688f84fce706d79e4002
Carlos E Alford <10241920+CarlosEAM@users.noreply.github.com>
2022-07-29 11:34:39 +0100

    Shopping list item create a li tag with text, delete, edit, icon

--- our-house/components/ShoppingListItem.js
+++ our-house/components/ShoppingListItem.js
@@ -1,10 +1,32 @@
 import React from 'react'
 import styles from '../styles/ShoppingListItem.module.css'
+import { FiEdit } from 'react-icons/fi'
+import { RiDeleteBin6Line } from 'react-icons/ri';
 
+/*
+Props needed
+ - text of the list item
+ - completed?
+ - profile icon stored as a number
 
-const ShoppingListItem = () => {
+Behviours
+ - edit button (icon)
+ - delete button (icon)
+*/
+const ShoppingListItem = ({name, completed, icon}) => {
   return (
-    <div>ShoppingListItem</div>
+    <li>
+      <div className={styles.left}>{name}</div>
+      <div className={styles.right}>
+        <img src="/user_avatar_1.svg" alt="user avatar icon" />
+        <button>
+          <FiEdit />
+        </button>
+        <button>
+          <RiDeleteBin6Line />
+        </button>
+      </div>
+    </li>
   )
 }
 
