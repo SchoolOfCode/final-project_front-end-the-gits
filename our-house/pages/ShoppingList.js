266a962be580cf1df39eefc9b5bee1c3030e1296
leeconroy <leeconroy1977@yahoo.co.uk>
2022-07-29 10:45:25 +0100

    producing list item when button is clicked

--- our-house/pages/ShoppingList.js
+++ our-house/pages/ShoppingList.js
@@ -3,22 +3,37 @@ import styles from '../styles/ShoppingList.module.css'
 import Navbar from '../components/Navbar.js'
 import InputBar from '../components/InputBar.js'
 import ShoppingListItem from '../components/ShoppingListItem.js'
-
+import { useState } from 'react'
 
 const ShoppingList = () => {
+
+  const [listItems, setListItems] = useState(["bread", "milk", "chocolate"])
+
+
+  const handleClick = (value) => {
+    setListItems([...listItems, value])
+
+  }
+
+
   const props = {
     name: "Lidl"
   }
-  const items = ["bread", "milk", "chocolate"]
+  
 
   return (
     <div className={styles.shoppinglist}>
     <Navbar/>
     <h1>{props.name} Shopping List</h1>
-    <InputBar/>
+    <input type="text"/>
+    <button onClick={ () => {
+      const value = document.querySelector('input')
+      handleClick(value.value)
+    }}></button>
+    {/* <InputBar handleClick={handleClick}/> */}
     <ul>
-      {items.map((item) => (
-        <ShoppingListItem name={item} key={item.id}/>))}
+      {listItems.map((item, index) => (
+        <ShoppingListItem name={item} key={index}/>))}
     </ul>
 
     </div>
