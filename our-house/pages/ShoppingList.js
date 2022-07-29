4bb5dc0d2aa8605e5ccb0145d56ef4366b9071e7
leeconroy <leeconroy1977@yahoo.co.uk>
2022-07-29 12:06:36 +0100

    delete function added and id added to props

--- our-house/pages/ShoppingList.js
+++ our-house/pages/ShoppingList.js
@@ -6,11 +6,27 @@ import ShoppingListItem from '../components/ShoppingListItem.js'
 import { useState } from 'react'
 
 const ShoppingList = () => {
-  const [listItems, setListItems] = useState(["bread", "milk", "chocolate"])
+  const [listItems, setListItems] = useState([
+    {name: "Bread", id:"1", completed: false, icon:"user_avatar_1.svg"},
+    {name: "Milk", id:"2", completed: false, icon:"user_avatar_1.svg"}])
 
   // takes in a value from the input component
   const updateShoppingList = (value) => {
-    setListItems([...listItems, value])
+    setListItems([value, ...listItems])
+  }
+
+  const deleteListItem = (id) => {
+    const newListItems = listItems.filter((item) => {
+      if (item.id === id) {
+        return false
+
+      }
+      else {
+        return true
+      }
+    }
+    )
+    setListItems(newListItems)
   }
 
   const props = {
@@ -30,7 +46,7 @@ const ShoppingList = () => {
     {/* <InputBar updateShoppingList={updateShoppingList}/> */}
     <ul>
       {listItems.map((item, index) => (
-        <ShoppingListItem name={item} key={index}/>))}
+        <ShoppingListItem name={item.name} key={index} id={item.id} deleteListItem={deleteListItem}/>))}
     </ul>
 
     </div>
