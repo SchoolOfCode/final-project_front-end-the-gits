58161a018a064a6313b24c98de9846abd3e66999
leeconroy <leeconroy1977@yahoo.co.uk>
2022-08-01 23:19:38 +0100

    css completed for large, meduim and iphone 8, small change to the main avatar div with row reversal

--- our-house/pages/index.js
+++ our-house/pages/index.js
@@ -6,21 +6,24 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.home}>
+        <div className={styles.right}>
+        
+        <img src="/logo_8.svg" alt="" />
+        </div>
       
         <div className={styles.left}>
           <h1>Our House</h1>
-          <p>Your one stop shop</p>
-          <p>to organise your home</p>
+          <div className={styles.text}>
+            <p>Your one stop shop</p>
+            <p>to organise your home</p>
+          </div>
           <div className={styles.buttons}>
-          <button>Login</button>
-          <button className={styles.signup}>Sign Up</button>
+            <button>Login</button>
+            <button className={styles.signup}>Sign Up</button>
           </div>
         </div>
         
-        <div className={styles.right}>
         
-          <img src="/logo_8.svg" width={900} height={1100} alt="" />
-        </div>
       </div>
   
       
