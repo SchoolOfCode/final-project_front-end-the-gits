8dd389a6dbd2dfdb6480dc9215791d79c8436c8f
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2022-08-04 14:21:02 +0100

    uncommented out Layout in _app.js

--- our-house/pages/_app.js
+++ our-house/pages/_app.js
@@ -1,14 +1,14 @@
 // pages/_app.js
 import React from 'react';
 import { UserProvider } from '@auth0/nextjs-auth0';
-// import { Layout } from '../components/Layout';
+import { Layout } from '../components/Layout';
 
 export default function App({ Component, pageProps }) {
   return (
     <UserProvider>
-      {/* <Layout> */}
+      <Layout>
         <Component {...pageProps} />
-      {/* </Layout> */}
+      </Layout>
     </UserProvider>
   );
 }
