b2934ddc75256fe3c18c0f2ed4535212a7ef0b1c
Abdullahi <amohamoud1999@gmail.com>
2022-08-05 15:13:57 +0100

    logout button

--- our-house/pages/api/auth/[...auth0].js
+++ our-house/pages/api/auth/[...auth0].js
@@ -1,16 +1,17 @@
-// // pages/api/auth/[...auth0].js
-// import { handleAuth } from '@auth0/nextjs-auth0';
-
-// console.log('AUTH0_SECRET = ', process.env.AUTH0_SECRET)
-
-// export default handleAuth();
-
+// pages/api/auth/[...auth0].js
 import { handleAuth, handleLogin } from "@auth0/nextjs-auth0";
 
+
+console.log('AUTH0_SECRET = ', process.env.AUTH0_SECRET)
 export default handleAuth({
   async login(req, res) {
     await handleLogin(req, res, {
       returnTo: "/UserHome",
     });
+    var options = {
+        theme: {
+          logo: 'https://thumbs2.imgbox.com/d3/92/hIE71HWq_t.png'
+        }
+      };
   },
 });
