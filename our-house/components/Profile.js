c7c21b702b50b8cf8fd1f669d9e6b1aa0c5c4c6c
Abdullahi <amohamoud1999@gmail.com>
2022-08-09 14:42:03 +0100

    metadata

--- our-house/components/Profile.js
+++ our-house/components/Profile.js
@@ -4,6 +4,38 @@ import styles from "../styles/Profile.module.css"
 
 export default function Profile() {
   const { user, error, isLoading } = useUser();
+  const [userMetadata, setUserMetadata] = useState(null);
+
+  useEffect(() => {
+    const getUserMetadata = async () => {
+      const domain = "dev-addbc3gj.us.auth0.com";
+  
+      try {
+        const accessToken = await getAccessTokenSilently({
+          audience: `https://${domain}/api/v2/`,
+          scope: "read:current_user",
+        });
+  
+        const userDetailsByIdUrl = `https://${domain}/api/v2/users/${user.sub}`;
+  
+        const metadataResponse = await fetch(userDetailsByIdUrl, {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        });
+  
+        const { user_metadata } = await metadataResponse.json();
+  
+        setUserMetadata(user_metadata);
+      } catch (e) {
+        console.log(e.message);
+      }
+    };
+  
+    getUserMetadata();
+  }, [getAccessTokenSilently, user?.sub]);
+
+  console.log(user)
 
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>{error.message}</div>;
