c13b95b859901d1ecf08baec9b976c121149150b
Carlos E Alford <10241920+CarlosEAM@users.noreply.github.com>
2022-08-11 00:13:19 +0100

    Update component to use userMeta context provider with custom className

--- our-house/components/Layout.js
+++ our-house/components/Layout.js
@@ -3,9 +3,8 @@ import Navbar from './Navbar'
 // import styles from '../styles/Profile.module.css'
 import { useRouter } from "next/router";
 import Profile from './Profile';
-
 import Footer from "./Footer";
-
+import { UserMetaProvider } from '../utils/UserMetaContext';
 
 
 export const Layout = ({ children }) => {
@@ -13,9 +12,6 @@ export const Layout = ({ children }) => {
   const router = useRouter();
 
   // on landing page dont load the navbar or profile bar
-
- 
-
   if (router.pathname === '/' || router.pathname === '/UserProfile' ) {
     return (
       <div>{children}
@@ -25,20 +21,14 @@ export const Layout = ({ children }) => {
   } else {
     // load navbar and profile bar
     return (
-      <div>
-      {/* //   <div className={styles.profile}>
-      //     <div className={styles.bar}>
-      //       <div className={styles.right}>
-      //         <h2> Welcome back, Lee</h2>   
-      //       </div>
-      //       <img src='/user_avatar_1.svg' width={20} height={70} alt='lee' />
-      //     </div>
-      //   </div> */}
-        <Profile />
-        <Navbar/>
-        {children}
-        <Footer />
-      </div>
+      <UserMetaProvider>
+        <div>
+          <Profile />
+          <Navbar/>
+          {children}
+          <Footer />
+        </div>
+      </UserMetaProvider>
     )
   }
 
