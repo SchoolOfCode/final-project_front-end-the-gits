4998e314bbfdbb602516c648f47c6223945c3976
Carlos E Alford <10241920+CarlosEAM@users.noreply.github.com>
2022-08-10 01:09:59 +0100

    Add API endpoint to get user metadata

---
+++ our-house/pages/api/userData/index.js
@@ -0,0 +1,28 @@
+import { withApiAuthRequired, getAccessToken } from "@auth0/nextjs-auth0";
+
+
+export default withApiAuthRequired(async function shows(req, res) {
+  // request fresh token
+  const { accessToken } = await getAccessToken(req, res, {});
+  // grab the user metadata object and user id
+  const {user_metadata, user_id} = req.body;
+  // using the Management API update the user_metadata
+  const response = await fetch(`https://dev-addbc3gj.us.auth0.com/api/v2/users/${user_id}`, {
+    method: 'PATCH',
+    headers: {
+      authorization: `Bearer ${accessToken}`,
+      'content-type': 'application/json'
+    },
+    body: JSON.stringify({user_metadata})
+  })
+
+  const data = await response.json();
+
+  // catch update error
+  if (data.statusCode >= 300) {
+    res.json({statusCode: data.statusCode, message: data.message});
+  }
+
+  res.status(200).json({user_metadata: data.user_metadata})
+
+});
