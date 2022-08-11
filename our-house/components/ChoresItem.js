f2a8acedb7e81033308b7d3ece344dcc42a8eadb
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2022-08-11 15:27:46 +0100

    added user icon to chores page

--- our-house/components/ChoresItem.js
+++ our-house/components/ChoresItem.js
@@ -1,48 +1,55 @@
-import React from 'react'
-import styles from '../styles/ChoresItem.module.css'
-import { FiEdit } from 'react-icons/fi'
-import { RiDeleteBin6Line } from 'react-icons/ri';
-import { GiRecycle } from 'react-icons/gi';
+import React from "react";
+import styles from "../styles/ChoresItem.module.css";
+import { RiDeleteBin6Line } from "react-icons/ri";
+import { useUser } from "@auth0/nextjs-auth0";
 
 const ChoresItem = ({
-    name,
-    completed,
-    icon,
-    id,
-    deleteListItem,
-    toggleItemAsCompleted,
-  }) => {
-  
-    // to style the list item when completed
-    let markAsDone = completed ? 'completed' : 'todo';
+  name,
+  completed,
+  id,
+  deleteListItem,
+  toggleItemAsCompleted,
+}) => {
+  // to style the list item when completed
+  let markAsDone = completed ? "completed" : "todo";
+  const { user, error, isLoading } = useUser();
 
-  
-    console.log(completed)
-  
-    return (
-      <li className={`${styles.shoppingListItem} ${styles[markAsDone]} theme-chores-item`}>
-        <div className={styles.left}>
-          <span key={id} onClick={() => { toggleItemAsCompleted(id); }}>
-            {name}
-          </span>
-        </div>
-        <div className={styles.right}>
-         <p className={styles.createdBy}>Created by</p>
-          <div className={styles.background}>
-            <img src="/user_avatar_1.svg" alt="user avatar icon" />
-          </div>
-          <div
-            className={styles.delete}
-            onClick={() => {
-              deleteListItem(id);
-            }}
-          >
-            <RiDeleteBin6Line />
-          </div>
-        </div>
-      </li>
-    );
-  };
-  
-  export default ChoresItem;
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
 
+  return (
+    <li
+      className={`${styles.shoppingListItem} ${styles[markAsDone]} theme-chores-item`}
+    >
+      <div className={styles.left}>
+        <span
+          key={id}
+          onClick={() => {
+            toggleItemAsCompleted(id);
+          }}
+        >
+          {name}
+        </span>
+      </div>
+      <div className={styles.right}>
+        <p className={styles.createdBy}>Created by</p>
+        <div
+          className={styles.background}
+          style={{ backgroundColor: user.user_metadata.theme_id }}
+        >
+          <img src={user.user_metadata.avatar_id} alt="user avatar icon" />
+        </div>
+        <div
+          className={styles.delete}
+          onClick={() => {
+            deleteListItem(id);
+          }}
+        >
+          <RiDeleteBin6Line />
+        </div>
+      </div>
+    </li>
+  );
+};
+
+export default ChoresItem;
--- our-house/components/ShoppingListItem.js
+++ our-house/components/ShoppingListItem.js
@@ -1,14 +1,11 @@
 import React, { useState } from "react";
 import styles from "../styles/ShoppingListItem.module.css";
-import { FiEdit } from "react-icons/fi";
 import { RiDeleteBin6Line } from "react-icons/ri";
 import { useUser } from "@auth0/nextjs-auth0";
-import { GiRecycle } from "react-icons/gi";
 
 const ShoppingListItem = ({
   name,
   completed,
-  icon,
   id,
   deleteListItem,
   toggleItemAsCompleted,
@@ -19,20 +16,28 @@ const ShoppingListItem = ({
   if (error) return <div>{error.message}</div>;
 
   // to style the list item when completed
-  let markAsDone = completed ? 'completed' : 'todo';
+  let markAsDone = completed ? "completed" : "todo";
 
   return (
-    <li className={`${styles.shoppingListItem} ${styles[markAsDone]} theme-shoppinglist-item`}>
+    <li
+      className={`${styles.shoppingListItem} ${styles[markAsDone]} theme-shoppinglist-item`}
+    >
       <div className={styles.left}>
-        <p key={id} onClick={() => { toggleItemAsCompleted(id); }}>
+        <p
+          key={id}
+          onClick={() => {
+            toggleItemAsCompleted(id);
+          }}
+        >
           {name}
         </p>
       </div>
       <div className={styles.right}>
-
-        <div className={styles.background} style={{ backgroundColor: user.user_metadata.theme_id }}>
+        <div
+          className={styles.background}
+          style={{ backgroundColor: user.user_metadata.theme_id }}
+        >
           <img src={user.user_metadata.avatar_id} alt="user avatar icon" />
-
         </div>
         <div
           className={styles.delete}
