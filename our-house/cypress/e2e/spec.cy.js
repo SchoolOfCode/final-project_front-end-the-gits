0414a3f937292976c6a080d41f11f7d0dd77a918
Carlos E Alford <10241920+CarlosEAM@users.noreply.github.com>
2022-08-11 16:19:23 +0100

    Comment the cypress tests

--- our-house/cypress/e2e/spec.cy.js
+++ our-house/cypress/e2e/spec.cy.js
@@ -1,3 +1,4 @@
+// Check the home page has the right header and login button
 context("Home Page", () => {
   beforeEach(()=>{
     cy.visit("https://final-project-front-end-the-gits.vercel.app/");
@@ -11,26 +12,32 @@ context("Home Page", () => {
   })
 });
 
-context("Auth0 login", ()=>{
+context("MVP walk through with Auth0", ()=>{
   beforeEach(()=>{
+    // run custom command to login on every test
     cy.login(Cypress.env("EMAIL"), Cypress.env("PASSWORD"));
   });
+
+  // Simply test the app logs in successfully
   it("testing auth0 login works correctly", ()=>{
     cy.visit("https://final-project-front-end-the-gits.vercel.app/UserHome")
     cy.get("h2");
   })
 
+  // User can click on the card to go into the shopping lists page
   it("test user can click on the shopping list card", () => {
     cy.visit("https://final-project-front-end-the-gits.vercel.app/UserHome");
     cy.get('a[href="/ShopName"]').first().click()
   })
 
+  // once in the shop lists user can create a new one
   it("test the input so the user can create lists", () => {
     cy.visit("https://final-project-front-end-the-gits.vercel.app/ShopName");
     cy.get('input[type="text"]').type('Tesco');
     cy.get("button").contains('Add').click();
   })
 
+  // inside the shop list a user can add items
   it("test the input so the user can add items to the shop list", () => {
     cy.visit("https://final-project-front-end-the-gits.vercel.app/ShopName");
     cy.wait(1000)
@@ -40,6 +47,7 @@ context("Auth0 login", ()=>{
     cy.get("li p").contains('Apples');
   })
 
+  // tests that the previously created item can be removed
   it("the user can remove single item from shop list", () => {
     cy.visit("https://final-project-front-end-the-gits.vercel.app/ShopName");
     cy.wait(1000)
@@ -47,6 +55,17 @@ context("Auth0 login", ()=>{
     cy.get("li p").contains('Apples');
     cy.get("li:last-child() div div svg").parent().last().click();
     cy.get("li p").contains('Apples').should('not.exist')
+  })
+
+  // list items can be marked as done
+  it("the user can mark single item as completed", () => {
+    cy.visit("https://final-project-front-end-the-gits.vercel.app/ShopName");
+    cy.wait(1000)
+    cy.get('a').last().click();
+    cy.get(".theme-shoppinglist-item").first().should('have.class', 'undefined')
+    cy.get(".theme-shoppinglist-item p").first().click();
+    cy.get(".theme-shoppinglist-item").first().not('have.class', 'undefined');
+    cy.get(".theme-shoppinglist-item p").first().click();
   })
 })
 
