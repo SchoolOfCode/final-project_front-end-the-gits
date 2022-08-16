09584e10de0c67ebd03f31785414787a6f1e85e2
Carlos E Alford <10241920+CarlosEAM@users.noreply.github.com>
2022-08-16 13:43:28 +0100

    Added further cypress tests

--- our-house/cypress/e2e/spec.cy.js
+++ our-house/cypress/e2e/spec.cy.js
@@ -44,19 +44,18 @@ context("MVP walk through with Auth0", ()=>{
     cy.visit("https://final-project-front-end-the-gits.vercel.app/ShopName");
     cy.wait(1000)
     cy.get('a').last().click();
+    // add some apples
     cy.get('input[type="text"]').type('Apples');
     cy.get("button").contains('Add').click();
     cy.get("li p").contains('Apples');
-  })
-
-  // tests that the previously created item can be removed
-  it("the user can remove single item from shop list", () => {
-    cy.visit("https://final-project-front-end-the-gits.vercel.app/ShopName");
-    cy.wait(1000)
-    cy.get('a').last().click();
-    cy.get("li p").contains('Apples');
-    cy.get("li:last-child() div div svg").parent().last().click();
-    cy.get("li p").contains('Apples').should('not.exist')
+    // add some Tomato sauce
+    cy.get('input[type="text"]').type('Tomato sauce');
+    cy.get("button").contains('Add').click();
+    cy.get("li p").contains('Tomato sauce');
+    // add some Orange juice
+    cy.get('input[type="text"]').type('Orange juice');
+    cy.get("button").contains('Add').click();
+    cy.get("li p").contains('Orange juice');
   })
 
   // list items can be marked as done
@@ -64,10 +63,34 @@ context("MVP walk through with Auth0", ()=>{
     cy.visit("https://final-project-front-end-the-gits.vercel.app/ShopName");
     cy.wait(1000)
     cy.get('a').last().click();
+    // mark first item as done
     cy.get(".theme-shoppinglist-item").first().should('have.class', 'undefined')
     cy.get(".theme-shoppinglist-item p").first().click();
+    // mark last item as done
+    cy.get(".theme-shoppinglist-item:last-child").first().should('have.class', 'undefined')
+    cy.get(".theme-shoppinglist-item:last-child p").first().click();
+    // uncheck first item as done
     cy.get(".theme-shoppinglist-item").first().not('have.class', 'undefined');
     cy.get(".theme-shoppinglist-item p").first().click();
+  })
+
+  // tests that the previously created item can be removed
+  it("the user can remove single item from shop list", () => {
+    cy.visit("https://final-project-front-end-the-gits.vercel.app/ShopName");
+    cy.wait(1000)
+    cy.get('a').last().click();
+    // remove the orange juice
+    cy.get("li p").contains('Orange juice');
+    cy.get("li:last-child() div div svg").parent().last().click();
+    cy.get("li p").contains('Orange juice').should('not.exist')
+    // remove the tomato sauce
+    cy.get("li p").contains('Tomato sauce');
+    cy.get("li:last-child() div div svg").parent().last().click();
+    cy.get("li p").contains('Tomato Sauce').should('not.exist')
+    // remove the apples
+    cy.get("li p").contains('Apples');
+    cy.get("li:last-child() div div svg").parent().last().click();
+    cy.get("li p").contains('Apples').should('not.exist')
   })
 })
 
